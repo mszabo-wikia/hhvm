7506b709b1c5dbeae05273f3d1da61fb550da690
Máté Szabó <mszabo@slack-corp.com>
2025-10-10 03:10:11 -0400

    Consolidate security-related compiler flags
    Update and extend the list of hardening-related compiler flags
    used by HHVM to better represent modern distro defaults.
    
    * Convert the existing `ENABLE_SSP` build option into a new
      `ENABLE_HARDENING` option and put an updated list of security flags behind it.
      Both clang and GCC have been supporting these options for a while now,
      so we can set them irrespective of the compiler.
    * Put PIE-related options behind a separate `ENABLE_PIE` build option
      so that we can produce and compare non-PIE and PIE builds once we fix
      compatibility with PIE.
    * Forward `CMAKE_BUILD_TYPE` to the mcrouter extension. Lack of this
      was causing mcrouter to be built without compiler optimizations,
      which doesn't play well with `FORTIFY_SOURCE`.
    
    HHVM runs fine with these flags, requiring only a small fix to folly/Subprocess.cpp
    to fix compatibility with the overloaded `openat()` syscall from `FORTIFY_SOURCE`.
    
    The overhead from these flags is likely to be limited, as many of them
    have been set by default for distribution packages for several years now.[1]
    
    [1] https://github.com/jvoisin/compiler-flags-distro

--- third-party/folly/src/folly/Subprocess.cpp
+++ third-party/folly/src/folly/Subprocess.cpp
@@ -147,6 +147,10 @@ static Ret subprocess_libc_load(
 #define FOLLY_DETAIL_SUBPROCESS_LIBC_X_OPEN(X) \
   X(open, __open_real)                         \
   X(openat, __openat_real)
+#elif defined(_FORTIFY_SOURCE) && defined(__clang__) && (__GLIBC__ > 2 || __GLIBC__ == 2 && __GLIBC_MINOR__ >= 40)
+#define FOLLY_DETAIL_SUBPROCESS_LIBC_X_OPEN(X) \
+  X(open, open)                                \
+  X(openat, __openat_2)
 #else
 #define FOLLY_DETAIL_SUBPROCESS_LIBC_X_OPEN(X) \
   X(open, open)                                \
