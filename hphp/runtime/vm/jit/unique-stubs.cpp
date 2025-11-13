e59a1f3b0c2fe55a9e4c28572cb603a957f6a5f2
Máté Szabó <mszabo@slack-corp.com>
2025-11-13 23:03:00 +0000

    Introduce helper function for loading LowTCA
    In #117, I changed back the hardcoded `loadzlq` instructions
    added by 1702bae to `emitLdPackedPtr` as it was before.
    That was wrong because LowTCA will be 64 bits on PIE builds
    but 32 bits otherwise, whereas `emitLdPackedPtr` uses the size
    of `PackedPtr` which is always 64 bits in non-lowptr builds.
    Pandemonium ensues as a result.
    
    So, introduce a new helper here that uses the correct size of LowTCA.

--- hphp/runtime/vm/jit/unique-stubs.cpp
+++ hphp/runtime/vm/jit/unique-stubs.cpp
@@ -300,7 +300,7 @@ TCA emitFuncPrologueRedispatch(CodeBlock& cb, DataBlock& data, const char* name)
     ifThen(v, CC_LE, sf, [&] (Vout& v) {
       // Fast path (numArgs <= numNonVariadicParams). Call the numArgs prologue.
       auto const dest = v.makeReg();
-      emitLdPackedPtr<uint8_t>(v, callee[numArgs * ptrSize + pTabOff], dest);
+      emitLdTCAPtr(v, callee[numArgs * ptrSize + pTabOff], dest);
       v << jmpr{dest, func_prologue_regs(true)};
     });
 
@@ -364,7 +364,7 @@ TCA emitFuncPrologueRedispatch(CodeBlock& cb, DataBlock& data, const char* name)
 
     // Call the numNonVariadicParams + 1 prologue.
     auto const dest = v.makeReg();
-    emitLdPackedPtr<uint8_t>(
+    emitLdTCAPtr(
       v,
       Vreg(r_func_prologue_callee())[numNewArgs * ptrSize + pTabOff],
       dest
@@ -426,7 +426,7 @@ TCA emitFuncPrologueRedispatchUnpack(CodeBlock& main, CodeBlock& cold,
     auto const pTabOff = safe_cast<int32_t>(Func::prologueTableOff());
     auto const ptrSize = safe_cast<int32_t>(sizeof(LowTCA));
     auto const dest = v.makeReg();
-    emitLdPackedPtr<uint8_t>(v, callee[numNewArgs * ptrSize + pTabOff], dest);
+    emitLdTCAPtr(v, callee[numNewArgs * ptrSize + pTabOff], dest);
     v << tailcallstubr{dest, func_prologue_regs(true)};
   }, name);
 
